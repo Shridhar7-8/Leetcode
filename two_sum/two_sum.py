06c0618c1aa07d2259f143fbad03d23db76a598a
Shridhar7-8 <shridharkur@gmail.com>
2026-01-20 00:19:42 +0530

    order of o(n)

--- two_sum/two_sum.py
+++ two_sum/two_sum.py
@@ -1,12 +1,10 @@
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
-        res=[]
         my_map = {}
-        for i in range(len(nums)):
-            my_map[i] = nums[i]
-            diff = target - nums[i]
-            if diff in my_map.values():
-                j = nums.index(diff)
-                if i!=j:
-                    res.extend((i,j))
-        return res
+        for i,num in enumerate(nums):
+            diff = target - num
+            if diff in my_map:
+                return [my_map[diff], i]
+            my_map[num] = i
+        return []
+        
